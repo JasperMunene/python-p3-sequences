1e2574412a63b1ab078959b03bdf9eb43f5aeb23
JasperMunene <jaspermunene2022@gmail.com>
2024-11-27 15:13:00 +0300

    Complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    a, b = 0, 1
+    fib_sequence = []
+
+    for _ in range(length):
+        fib_sequence.append(a)
+        a, b = b, a + b
+
+    print(fib_sequence)
